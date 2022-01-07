ad447d36564316f04d8d46550816c9847fb26132
Shashank Kumar <shashankkumarthakur@gmail.com>
2022-01-07 19:03:29 +0530

    👽️ Change api

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log("This is my index.js file")
+console.log("This is my index.js file");
 //26e92693046341ad9d594421f6e1b21d
 
 //Initialise the news parameters
-let source = 'bbc-news';
-let apikey = '26e92693046341ad9d594421f6e1b21d';
+let source = "bbc-news";
+let apikey = "26e92693046341ad9d594421f6e1b21d";
 // //Grab the newscontainer
-let newsAccordion = document.getElementById('newsAccordion');
+let newsAccordion = document.getElementById("newsAccordion");
 
 // //Create an ajax get request
 // const xhr = new XMLHttpRequest();
@@ -46,25 +46,28 @@ let newsAccordion = document.getElementById('newsAccordion');
 // xhr.send();
 
 async function fectchNewsItems() {
-    let newsHTML ="";
-    fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apikey}`
-    ).then((response) => 
-    {
-        response.json().then((data)=> 
-        {
-            data.articles.forEach(function (element, index) {
+    let newsHTML = "";
+    fetch(`https://alexa-news-api.herokuapp.com/`).then((response) => {
+        response.json().then((data) => {
+            data.data.forEach(function (element, index) {
                 let news = `<div class="card">
                         <div class="card-header" id="heading${index}">
                             <h2 class="mb-0">
                             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
                                 aria-expanded="false" aria-controls="collapse${index}">
-                            <b>Breaking News ${index + 1}:</b> ${element["title"]}
+                            <b>Breaking News ${index + 1}:</b> ${
+                    element["title"]
+                }
                             </button>
                             </h2>
                         </div>
                 
                         <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
-                            <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            <div class="card-body"> ${
+                                element["content"]
+                            }. <a href="${
+                    element["url"]
+                }" target="_blank" >Read more here</a>  </div>
                         </div>
                         </div>`;
                 newsHTML += news;
@@ -74,4 +77,4 @@ async function fectchNewsItems() {
     });
 }
 
-window.addEventListener('load', fectchNewsItems);
+window.addEventListener("load", fectchNewsItems);
